acc24dfe11c08b51154cc92fbc31371ead606324
ShahlaMva <shahlanm@code.edu.az>
2021-09-01 11:46:46 +0400

    Add login page

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+$(document).on("focus",".register-input",function(){
+    $(this).css('outline','none');
+
+
+});
+// $(document).on("click",".tabs a",function(e){
+// e.preventDefault();
+
+// let contents=$("#register .content");
+// for (let tab of contents) {
+//     console.log(tab.value);
+//     if($(this).parent().attr('id')==tab){
+//         console.log("ok");
+//     }
+//     else{
+//         console.log("no");
+//     }
+// }
+// if($(this).parent().hasClass('active')){ 
+//   $('.tabs tab-button').addClass('active');           
+//   $(this).parent().removeClass('active');
+
+});
