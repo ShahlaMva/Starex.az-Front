fcc0155ee5376e17c49e5aa12165a158ded86383
ShahlaMva <shahlanm@code.edu.az>
2021-09-02 13:54:56 +0400

    add tabs menu

--- js/script.js
+++ js/script.js
@@ -1,25 +1,52 @@
 $(document).ready(function() {
-
+//input when click remove border 
 $(document).on("focus",".register-input",function(){
     $(this).css('outline','none');
 
 
 });
-// $(document).on("click",".tabs a",function(e){
-// e.preventDefault();
+//register page tabs
+$(document).on("click",".tabs a",function(e){
+e.preventDefault();
 
-// let contents=$("#register .content");
-// for (let tab of contents) {
-//     console.log(tab.value);
-//     if($(this).parent().attr('id')==tab){
-//         console.log("ok");
-//     }
-//     else{
-//         console.log("no");
-//     }
-// }
-// if($(this).parent().hasClass('active')){ 
-//   $('.tabs tab-button').addClass('active');           
-//   $(this).parent().removeClass('active');
+if($(this).parent().attr("id")=="business"){
+    $(this).parent().addClass('active');
+    
+        $("#person").removeClass("active");   
+     $("#business1").removeClass("d-none");
+       $("#person1").hide();
+   
+}else{
+    $(this).parent().addClass('active');
+  $("#business").removeClass("active");
+    $("#business1").addClass("d-none");
+  
+    $("#person1").show();
+  
+}
+
+});
+//shop tabs
+$(document).on("click",".tabs-list .tab-shop a",function(e){
+e.preventDefault();
+$(this).parent().addClass("border");
+ $(this).parent().siblings().removeClass("border");
+ if($(this).parent().attr("id")=="chine"){
+   $("#chine-content").removeClass("d-none");
+   $("#chine-content").siblings().addClass("d-none");
+ }else if($(this).parent().attr("id")=="germany"){
+  $("#germany-content").removeClass("d-none");
+  $("#germany-content").siblings().addClass("d-none");
+ }else if($(this).parent().attr("id")=="turkey"){
+  $("#turkey-content").removeClass("d-none");
+  $("#turkey-content").siblings().addClass("d-none");
+ }else{
+  $("#usa-content").removeClass("d-none");
+  $("#usa-content").siblings().addClass("d-none");
+ }
+
+
+
+})
 
 });
